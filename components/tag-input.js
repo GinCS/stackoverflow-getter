cc26ffdee5c3a1b8c362676a1939efff1cbd7300
GinCS <GinnyCStudent@gmail.com>
2021-03-08 20:47:20 -0600

    made tag-input component and [tag].js using next.js Server Side Rendering implemented

--- components/tag-input.js
+++ components/tag-input.js
@@ -1,13 +1,32 @@
 import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.css'
 
+
 export const SearchTag = () => {
+    var tag = "";
+
     const router = useRouter();
+
+    const updateTag = (event) => {
+        tag = event.target.value;
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (tag) {
+            router.push(`/result/${tag}`);
+        }
+    }
+    
     return (
         <div>
-            <form>
-                <input className={styles.description} type="text" placeholder="Enter A Tag" name="tag" required />
-                <button onClick={()=> router.push('/result/'+'tag') } className={styles.tag}>Submit</button>
+            <form onSubmit={onSubmit} className={styles.tagin}>
+                <input className={styles.description}
+                    type="text"
+                    placeholder="Enter A Tag"
+                    onChange={updateTag} 
+                    name="tag" required />
+                <button type="submit" className={styles.tag}>Submit</button>
             </form>
         </div>
 
--- pages/result/[tag].js
+++ pages/result/[tag].js
@@ -1,33 +1,50 @@
+import { SearchTag } from '../../components/tag-input';
+import Head from 'next/head';
+import styles from '../../styles/Home.module.css';
+
 export const Result = ({ tag, questions }) => {
-    return (<>Hey~</>);
+    console.log(tag, questions);
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Result</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    <a href="https://stackoverflow.com/" target="_blank">Stack Overflow</a> Tag Getter
+                </h1>
+                <SearchTag />
+                <h2>Tag: {tag}</h2>
+            </main>
+
+        </div>
+    );
 };
 
-export const getServerSideProps = async pageContext => {
-    const tag = pageContext.query.tag;
+export async function getServerSideProps (pageContext) {
+    const tagged = pageContext.query.tag;
 
-    if (!tag) {
+    if (!tagged) {
         return {
             props: {
                 questions: [],
-                tag: ''
+                tag: 'null'
             }
         }
     }
 
     const apiResponse = await fetch(
-        `https://api.stackexchange.com/docs/search#order=desc&sort=votes&tagged=${tag}&filter=default&site=stackoverflow&run=true`,
-        {
-            headers: {
-            },
-        },
+        `https://api.stackexchange.com/2.2/search?fromdate=1612742400&todate=1615161600&order=desc&sort=votes&tagged=${tagged}&site=stackoverflow`
     );
 
-    const apiJSON = apiResponse.json();
+    const apiJSON = await apiResponse.json();
     console.log(apiJSON);
-
     return {
         props: {
-
+            tag: tagged,
+            questions: apiJSON
         }
     }
 };
