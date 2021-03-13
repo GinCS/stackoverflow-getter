15db482ebcdc01e73fd57d06c4699dd8f1a94a54
GinCS <GinnyCStudent@gmail.com>
2021-03-12 21:02:30 -0600

    Completed displaying

--- pages/result/[tag].js
+++ pages/result/[tag].js
@@ -1,9 +1,12 @@
 import { SearchTag } from '../../components/tag-input';
 import Head from 'next/head';
 import styles from '../../styles/Home.module.css';
+import React from 'react';
+import Collapsible from 'react-collapsible';
 
 export const Result = ({ tag, votes, creations }) => {
     console.log(tag, votes, creations);
+    
 
     let mergedList = [];
     let sizeForVotes = 10, sizeForCreations = 10;
@@ -44,7 +47,10 @@ export const Result = ({ tag, votes, creations }) => {
         return result;
     }
 
-    
+    function bodyBreakdown(item) {
+        return { __html: item.body.replace(/\n/g, "<br/>") };//body_markdown.replace(/\n/g,"<br/>")};
+    }
+
 
     return (
         <div className={styles.container}>
@@ -60,6 +66,7 @@ export const Result = ({ tag, votes, creations }) => {
                 <SearchTag />
                 <h2>Tag: {tag}</h2>
                 {mergedList.map(function (item, index) {
+                    
                     if (error) {return}
                     let txt = item.title;
 
@@ -71,11 +78,19 @@ export const Result = ({ tag, votes, creations }) => {
                     const date = new Date(item.creation_date * 1000);
                     
                     return (
-                        <button key={index} type="button" className={styles.collapsable}>
+                        <Collapsible key={index} className={styles.collapseOuter} trigger={
+                            (<button type="button" className={styles.collapsable} >
                             Creation Date: {date.getFullYear()}/{date.getMonth() + 1}/{date.getDate()}
-                            <br /> {txt.value} <br /> Votes: {item.score}
-                        </button>
-                        )
+                            <br /> {txt.value} <br /> Votes: {item.score}</button>)}
+                        >
+                                <div className={styles.collapseInner}>
+                                    <h3>Question Body</h3>
+                                    <div className={styles.qBody} dangerouslySetInnerHTML={bodyBreakdown(item)} />
+                                <h4>Question Comments</h4>
+
+                                </div>
+                        </Collapsible>
+                    );
                 })}
             </main>
 
@@ -85,6 +100,9 @@ export const Result = ({ tag, votes, creations }) => {
 
 export async function getServerSideProps (pageContext) {
     const tagged = pageContext.query.tag;
+    // filter for body: !)rTkraPXxg*xgr03n8Uq
+    // filter for body_markdown: !LYA)NnjOEl60aBgzZ1(2jI
+    const filter = "!)rTkraPXxg*xgr03n8Uq";
     const currTime = Math.floor(Date.now()/1000);
     const weekBefore = currTime - 604800;
     if (!tagged) {
@@ -96,15 +114,17 @@ export async function getServerSideProps (pageContext) {
         }
     }
 
+    
+
     let apiResponse = await fetch(
-        `https://api.stackexchange.com/2.2/search?fromdate=${weekBefore}&todate=${currTime}&order=desc&sort=votes&tagged=${tagged}&filter=!)rTkraPXxg*xgr03n8Uq&site=stackoverflow&key=${process.env.STACK_EXCHANGE_KEY}`
+        `https://api.stackexchange.com/2.2/search?fromdate=${weekBefore}&todate=${currTime}&order=desc&sort=votes&tagged=${tagged}&filter=${filter}&site=stackoverflow&key=${process.env.STACK_EXCHANGE_KEY}`
     );
 
 
     const apiVotesJSON = await apiResponse.json();
 
     apiResponse = await fetch(
-        `https://api.stackexchange.com/2.2/search?fromdate=${weekBefore}&todate=${currTime}&order=desc&sort=creation&tagged=${tagged}&filter=!)rTkraPXxg*xgr03n8Uq&site=stackoverflow&key=${process.env.STACK_EXCHANGE_KEY}`
+        `https://api.stackexchange.com/2.2/search?fromdate=${weekBefore}&todate=${currTime}&order=desc&sort=creation&tagged=${tagged}&filter=${filter}&site=stackoverflow&key=${process.env.STACK_EXCHANGE_KEY}`
     );
 
     const apiCreationJSON = await apiResponse.json();
